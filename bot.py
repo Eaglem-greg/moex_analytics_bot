67ce5e4a7372fc9b5b5992fdd70868686fccbf65
Gregory <gregory.novikov@inbox.ru>
2026-01-02 00:24:32 +0300

    MVP bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,41 @@
+import asyncio
+import logging
+import os
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+from dotenv import load_dotenv
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+if not BOT_TOKEN:
+    logger.error("BOT_TOKEN is not in .env")
+    exit(1)
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+async def process_start_command(message: Message):
+    await message.answer("Бот запущен! Используй /help")
+
+async def process_help_command(message: Message):
+    text = "Команды:\n/start - бот запущен\n/help - помощь\n/quote SBER - цена"
+    await message.answer(text)
+
+async def process_quote_command(message: Message):
+    await message.answer('pass')
+
+dp.message.register(process_start_command, Command(commands='start'))
+dp.message.register(process_help_command, Command(commands='help'))
+dp.message.register(process_quote_command, Command(commands='quote'))
+
+async def main():
+    logger.info("Бот запускается...")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
 
